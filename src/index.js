d6fc320b5e94a39e6e884aa6d1fc71d5034f4ffe
BlythePollard <github email address>
2020-05-02 15:23:47 -0600

    set up index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import App from './App';
 
 //set up store
-//any component that we wrap in provider will have access to 
+//any component or child that we wrap in provider will have access to 
 //redux store- wrap app in provider
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}> 
+    <App /> 
+   </Provider>,
   document.getElementById('root')
 );
