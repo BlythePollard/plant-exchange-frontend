6a4f1200720e3429e5df4ab1a1a17dc099035c51
BlythePollard <github email address>
2020-05-02 22:10:19 -0600

    added availableplant component to link show page for available plants

---
+++ src/components/AvailablePlant.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AvailablePlant = (props) => {
+    const isAvailable = props.isAvailable;
+    if(isAvailable == true) {
+        return (
+            <div>
+            <li>Link to show page here</li>
+            </div>
+        )
+    } else { return ( <div>Not Available</div>)}
+}
+
+export default AvailablePlant
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,17 +2,19 @@
 //component for sake of requirements
 
 import React from 'react';
+import AvailablePlant from './AvailablePlant'
 
 export default class PlantList extends React.Component {
+
     render() {
-        console.log(this.props.plants)
         return(
             <div>
                 {this.props.plants.map(plant => 
                 <li key={plant.id}>
                     {plant.name}
                     <ul>{plant.description}</ul>
-                </li> //plus link to each show page via user
+                    <ul><AvailablePlant isAvailable={plant.available}/></ul>
+                </li> //plus link to each show page via user, only showing plants that are available
                 )}
             </div>
         )
