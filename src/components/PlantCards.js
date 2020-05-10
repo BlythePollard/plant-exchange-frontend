1e4cba18b124f6b7da5914cdd3ccff9d9a9b946a
BlythePollard <github email address>
2020-05-10 13:40:22 -0600

    implemented max 2 cards per row/column in PlantCards

--- src/components/PlantCards.js
+++ src/components/PlantCards.js
@@ -3,7 +3,10 @@ import { connect } from 'react-redux';
 import {deletePlant} from '../actions/deletePlant'
 import Navbar from '../components/Navbar'
 import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import CardDeck from 'react-bootstrap/CardDeck'
+import Col from 'react-bootstrap/Col'
 import '../App.css';
 
 
@@ -14,21 +17,29 @@ class PlantCards extends React.Component {
     }
 
     render() { 
-        console.log(this.props)
         return(
             <div> 
                 <Navbar/>
-                <CardDeck className="container">
+                <Container>
+                    <Row>
+                <CardDeck>
                 {this.props.userPlants.map(userPlant => 
-                <Card border="header" bg="light" key={userPlant.id}>
+                <Col >
+                <Card style={{ width: '18rem' }} border="header" bg="light" key={userPlant.id}>
                     <Card.Body>
                         <Card.Title>{userPlant.name}</Card.Title>
                         <Card.Text>{userPlant.description}</Card.Text>
                         <button onClick={() => this.handleDelete(userPlant)}>Delete</button>
                     </Card.Body>
-                </Card>
-                 )}
+                </Card> 
+                <br></br>
+                </Col>
+                )}
+                
                  </CardDeck>
+                 
+                 </Row>
+                 </Container>
             </div>
         )
     }
@@ -36,3 +47,19 @@ class PlantCards extends React.Component {
 
 export default connect(null, {deletePlant})(PlantCards)
 
+//<Col md={3}>     </Col>
+
+{/* <Row>
+            const numberOfRows = Math.ceil(this.props.userPlants.length / 3)
+
+Array(numberOfRows).fill().map((_, rowIndex) => (
+    <Row key={rowIndex}>
+     {
+       beersArray.slice(rowIndex * 3, (rowIndex *3) + 3).map(beer => (
+        <Col xs="12" sm="4">
+          <BeersListItem key={beer.id} beer={beer} />
+        </Col>
+      ))}
+    </Row>
+))
+</Row> */}
